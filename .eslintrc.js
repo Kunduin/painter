9489b54115d9e7aa1168e9a06be2c173a03da124
kunduin <15852158126@163.com>
2018-09-25 21:12:54 +0800

    commit config file

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: "babel-eslint"
+  },
+  env: {
+    browser: true
+  },
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    "plugin:vue/recommended",
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    "standard",
+    "plugin:prettier/recommended"
+  ],
+  // required to lint *.vue files
+  plugins: ["vue"],
+  // add your custom rules here
+  rules: {
+    "vue/html-self-closing": [
+      "error",
+      {
+        html: {
+          void: "never",
+          normal: "always",
+          component: "always"
+        },
+        svg: "always",
+        math: "always"
+      }
+    ],
+    // allow async-await
+    "generator-star-spacing": "off",
+    // allow debugger during development
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+  }
+};
